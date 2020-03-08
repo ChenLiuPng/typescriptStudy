77f0a9b02308c6e273e8853a62819c89be40c8bc
tail <vclp@163.com>
2020-03-09 02:20:22 +0800

    环境安装

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # typescriptStudy
 ts系统入门到实战 学习日志
+
+## 环境
+
+安装`vscode`插件 *prettier Code formatter*
+
+全局安装`typescript`
+
+```shell
+ npm i -g typescript
+ ```
+
+全局安装 `ts-node`
+> 用法: ts-node filename.ts
+
+```shell
+ npm i -g ts-node
+ ```
--- demo.ts
+++ demo.ts
@@ -3,21 +3,23 @@ let b: number = 123;
 // b = '123'; 报错
 b = 456;
 
-
-function tsDemo1 (data: {x:number, y:number}){
-    return Math.sqrt(data.x ** 2 + data.y ** 2);
+function tsDemo1(data: { x: number, y: number }) {
+	return Math.sqrt(data.x ** 2 + data.y ** 2);
 }
 
-tsDemo1({x:1, y:123});
+tsDemo1({ x: 1, y: 123 });
 
 // ts 静态类型使我们在编写代码时更能发现问题
 // 可以更能加强代码提示
 // {x:number,y:number}就能知道data里面就有x和y
 // 代码可读性
-interface Point {x:number, y:number};
-
-function tsDemo2 (data: Point){
-    return Math.sqrt(data.x ** 2 + data.y ** 2);
+interface Point {
+	x: number;
+	y: number;
 }
 
-tsDemo2({x:1, y:123});
+function tsDemo2(data: Point) {
+	return Math.sqrt(data.x ** 2 + data.y ** 2);
+}
+console.log('1');
+tsDemo2({ x: 1, y: 123 });
