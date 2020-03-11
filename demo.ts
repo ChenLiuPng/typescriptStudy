811dc72d29b9fc5d5cac72f93efd77b6b01c2b51
tail <vclp@163.com>
2020-03-11 21:28:54 +0800

    类型注解 类型推断

--- demo.ts
+++ demo.ts
@@ -57,3 +57,31 @@ const dell: Person = new Person();
 const getTatal: () => number = () => {
 	return 123;
 };
+
+// type annotation 类型注解 我们来告诉TS变量是什么类型
+
+let count2: number;
+
+count2 = 123;
+
+// type inference 类型推断  TS 会自动的去尝试分析变量的类型
+// 如果TS能够自动分析变量类型，我们就什么也不需要做了
+// 如果 TS 无法分析变量类型的话， 我们就需要使用类型注解
+
+let countInference = 123;
+
+const firstNumber = 1;
+const secondNumber = 2;
+const total = firstNumber + secondNumber;
+
+function getTatal1(firstNumber: number, secondNumber: number) {
+	return firstNumber + secondNumber;
+}
+const total1 = getTatal1(firstNumber, secondNumber);
+
+let eleId = document.getElementById('a');
+
+const obj = {
+	name: 'dell',
+	age: 18
+};
